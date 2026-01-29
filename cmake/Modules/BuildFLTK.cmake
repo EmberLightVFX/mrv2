d19a8f421ebb93d70a3a22e8b7808b7e34dbd0c0
Gonzalo Garramuño <ggarra13@gmail.com>
2026-01-29 07:43:52 -0300

    Bumped FLTK version.

--- cmake/Modules/BuildFLTK.cmake
+++ cmake/Modules/BuildFLTK.cmake
@@ -4,7 +4,7 @@
 
 include( ExternalProject )
 
-set(FLTK_GIT_TAG v1.5.8)  # was v1.5.6
+set(FLTK_GIT_TAG v1.5.9)  # was v1.5.6
 
 #set(FLTK_GIT_TAG vk)  # Cutting edge!
 #set(FLTK_GIT_TAG vk_merge) # Testing branch
